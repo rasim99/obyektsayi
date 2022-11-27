4512fbf56d820157d9e5d32ff7ef5c9f97c2c55d
rasim99 <rasimjm@code.edu.az>
2022-11-27 02:12:41 -0800

    obyektsayi

---
+++ Idhowmuch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Idhowmuch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person();
+            Console.WriteLine($"Count=>{Person.Count}");
+            
+           
+
+        }
+
+       
+    }
+}
---
+++ Idhowmuch/person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Idhowmuch
+{
+    class Person
+    {
+        public static int Count;
+        public Person()
+        {
+            Count++;
+        }
+
+    }
+}
